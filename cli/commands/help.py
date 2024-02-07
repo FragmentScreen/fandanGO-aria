26fd3354de04904cc7590d15b9672b493eb795f9
Lui Holliday <lui.holliday@instruct-eric.org>
2024-02-07 20:35:33 +0000

    updates to help command

--- cli/commands/help.py
+++ cli/commands/help.py
@@ -1,13 +1,17 @@
 import click
+from aria_data_deposition.utils import print_with_spaces
 
 @click.command()
 def help():
     """Display help information for Aria Data Deposition."""
-    click.echo("")
-    click.echo("Welcome to the Aria Data Deposition toolkit 👌")
+
+    print_with_spaces('Welcome to the ARIA Data Deposition toolkit.')
     click.echo('----------------------------------------------')
-    click.echo("Available commands:")
-    click.echo('')
-    click.echo(" aria-login : Login to the project and retrieve a token.")
-    click.echo(" aria-help  : Display help information.")
-    click.echo('')
+    click.echo('Available commands:')
+    print_with_spaces('')
+    click.echo('Welcome to the Aria Data Deposition toolkit 👌')
+    click.echo('----------------------------------------------')
+    click.echo(' aria-login : Login to the project and retrieve a token.')
+    click.echo(' aria-get   : Test Token handling')
+    click.echo(' aria-help  : Display help information.')
+    print_with_spaces('')
