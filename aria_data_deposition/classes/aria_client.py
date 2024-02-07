95a59b3f102ac0e14d9a9039a0127b4b6e66b436
Lui Holliday <lui.holliday@instruct-eric.org>
2024-02-07 21:36:54 +0000

    introduce client wrapper to super client class

--- aria_data_deposition/classes/aria_client.py
+++ aria_data_deposition/classes/aria_client.py
@@ -1,16 +1,17 @@
 from .oauth import OAuth
+from .client import Client
 from .do_something import DoSomething
 
 class AriaClient :
     def __init__(self) :
-        self.oauth = OAuth()
+        self.client = Client(OAuth())
 
     def login(self, username, password):
-        self.oauth.login(username, password)
+        self.client.authenticate(username, password)
 
-    def get_token(self) : 
-        return self.oauth.get_access_token()
+    def get_access_token(self) : 
+        return self.client.get_access_token()
     
     def do_something(self) : 
-        token = self.get_token()
+        token = self.get_access_token()
         DoSomething.dispatch_hook(token)
--- cli/cli.py
+++ cli/cli.py
@@ -2,6 +2,7 @@
 import click
 from cli.commands.login import login
 from cli.commands.help import help
+from cli.commands.set_token_password import set_token_password
 from aria_data_deposition.classes.aria_client import AriaClient
 
 @click.group()
@@ -14,6 +15,7 @@ aria_client_instance = AriaClient()
 
 cli.add_command(login)
 cli.add_command(help)
+cli.add_command(set_token_password)
 
 if __name__ == '__main__':
     cli()
