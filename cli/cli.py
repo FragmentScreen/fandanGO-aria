de58f25c7b01c04aaf0c7404b3ea6344f9af5c23
Lui Holliday <lui.holliday@instruct-eric.org>
2024-02-27 12:32:35 +0000

    add to cli

--- cli/cli.py
+++ cli/cli.py
@@ -3,6 +3,13 @@ import click
 from cli.commands.login import login
 from cli.commands.help import help
 from cli.commands.get_token_password import get_token_password
+from cli.commands.create_bucket import create_bucket
+from cli.commands.list_buckets import list_buckets
+from cli.commands.create_record import create_record
+from cli.commands.list_records import list_records
+from cli.commands.create_field import create_field
+from cli.commands.list_fields import list_fields
+
 from aria_data_deposition.classes.aria_client import AriaClient
 
 @click.group()
@@ -10,12 +17,16 @@ def cli():
     """Aria Data Deposition CLI."""
     pass
 
-
-aria_client_instance = AriaClient()
-
 cli.add_command(login)
 cli.add_command(help)
 cli.add_command(get_token_password)
+cli.add_command(create_bucket)
+cli.add_command(list_buckets)
+cli.add_command(create_record)
+cli.add_command(list_records)
+cli.add_command(create_field)
+cli.add_command(list_fields)
+
 
 if __name__ == '__main__':
     cli()
