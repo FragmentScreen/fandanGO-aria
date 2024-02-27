d089fca12fb5ce3b08c946bf75ede1217909ae75
Lui Holliday <lui.holliday@instruct-eric.org>
2024-02-27 12:33:41 +0000

    prevent token password check on login

--- cli/commands/login.py
+++ cli/commands/login.py
@@ -3,9 +3,9 @@ import click
 from aria_data_deposition.classes.aria_client import AriaClient
 
 @click.command()
-@click.option('--username', prompt='Your ARIA email address', help='Your ARIA email address', default='aria.test.admin@instruct-eric.eu')
-@click.option('--password', prompt=True, hide_input=True, help='Your password', default='')
+@click.option('--username', prompt='Your ARIA email address', help='Your ARIA email address', default='lui.holliday@instruct-eric.org')
+@click.option('--password', prompt=True, hide_input=True, help='Your password', default='4XSzzUq9SCe2#vkT')
 def login(username, password):
     """Login to ARIA and retrieve a token. Store in keyring."""
-    aria_cli = AriaClient()
+    aria_cli = AriaClient(True)
     aria_cli.login(username, password)
