0492f5fb92259aae625c16127a09d314371bcdfc
Lui Holliday <lui.holliday@instruct-eric.org>
2024-02-28 13:49:00 +0000

    streamline data_manger + field class

--- aria_data_deposition/classes/data_manager.py
+++ aria_data_deposition/classes/data_manager.py
@@ -1,81 +1,46 @@
 from ..config import * 
 from ..utils import pretty_print, set_headers
 from .bucket import Bucket
+from .record import Records
+from .field import Fields
 
 class DataManager:
-    def __init__(self):
-        self.bucket = Bucket()
+    def __init__(self, token):
+        self.token = token
+        self.bucket = Bucket(self.token)
+        self.records = Records(self.token)
+        self.fields = Fields(self.token)
 
-    # def create_bucket(self, fields, token) -> None :
-    #     # need to add click input 
-    #     fields = {'aria_id': 12, "aria_entity_type" : 'visit', 'embargoed_until' : '2040-02-19 12:00:00'}
-    #     headers = set_headers(token)
-    #     some_url = 'http://localhost:8281/api/v1/createDataBucket'
-    #     response = requests.post(some_url, json=fields, headers=headers)
-    #     response.raise_for_status()
-    #     resp = response.json()
-    #     pretty_print(resp)
-    #     pass
-        
-    def create_bucket(self, token) : 
-        self.bucket.create_bucket(token)
+    def create_bucket(self) : 
+        self.bucket.create_bucket()
+    
+    def list_buckets(self):
+        buckets = self.bucket.list_buckets()
+        pretty_print(buckets)
 
+    def list_records(self):
+        self.select_bucket()
+        self.records.get_records()
     
+    def create_record(self):
+        self.select_bucket()
+        self.records.create_record()
+
+    def select_bucket(self) : 
+        bucket = self.bucket.select_bucket()
+        self.records.bucket_id = bucket['id']
     
-    def list_buckets(self, token):
-        headers = set_headers(token)
-        some_url = 'http://localhost:8281/api/v1/bucket'
-        response = requests.get(some_url, headers=headers)
-        response.raise_for_status()
-        resp = response.json()
-        pretty_print(resp)
-        pass
+    def select_record(self) :
+        self.select_bucket()
+        record = self.records.select_record()
+        self.fields.record_id = record['id']
+
+    def create_field(self):
+        self.select_record()
+        self.fields.create_field()
     
-    def create_record(self, token, bucket_id='3274f598-6eb5-4209-bf9b-a0305f4835ee', record_data=None,):
-        some_url = 'http://localhost:8281/api/v1/createDataRecord'
-        print(token)
-        headers = set_headers(token)
-        schema = 'TestSchema'
-        context = None
-        resp = requests.post(some_url, {'schema' : schema, 'bucket': bucket_id}, headers=headers)
-        resp.raise_for_status()
-        resp = resp.json()
-        pretty_print(resp)
-        pass
-    
-    def list_records(self, token):
-        some_url = 'http://localhost:8281/api/v1/record'
-        headers = set_headers(token)
-        resp = requests.get(some_url, headers=headers)
-        resp.raise_for_status()
-        resp = resp.json()
-        pretty_print(resp)
-        pass
-    
-    def create_field(self, token):
-        some_url = 'http://localhost:8281/api/v1/createDataField'
-        headers = set_headers(token)
-        record = "d7f7be48-24c4-4440-83ca-dc46cff2c596"
-        type = "TestFieldType"
-        content = "hello lui"
-        content = json.dumps(content)
-        options = {"option" : "Lui"}
-        options = json.dumps(options)
-        stuff = {"record": "d7f7be48-24c4-4440-83ca-dc46cff2c596","content": content, "type": "TestFieldType", "options": options}
-        # stuff = json.dumps(stuff)
-        resp = requests.post(some_url, stuff, headers=headers)
-        # resp.raise_for_status()
-        resp = resp.json()
-        pretty_print(resp)
-        pass
-    
-    def list_fields(self, token):
-        some_url = 'http://localhost:8281/api/v1/field?record=d7f7be48-24c4-4440-83ca-dc46cff2c596'
-        headers = set_headers(token)
-        resp = requests.get(some_url, headers=headers)
-        resp.raise_for_status()
-        resp = resp.json()
-        pretty_print(resp)
-        pass
+    def list_fields(self):
+        self.select_record()
+        self.fields.list_fields()
 
 # may want to turn these into subclasses where bucket consumes record, consumes field 
---
+++ aria_data_deposition/classes/field.py
@@ -0,0 +1,53 @@
+from ..utils import *
+from ..config import *
+
+class Fields :
+
+    def __init__(self, token) : 
+        self.token = token
+        self.record_id = None
+        self.headers = set_headers(token)
+        self.options = ['TestFieldType', 'OtherFieldType']
+        self.list_url = f'http://localhost:8281/api/v1/field?filter[record]={self.record_id}'
+        self.create_url = 'http://localhost:8281/api/v1/createDataField'
+
+
+
+    def create_field(self):
+        fields = self.get_fields_data()
+        resp = requests.post(self.create_url, fields, headers=self.headers)
+        # resp.raise_for_status()
+        resp = resp.json()
+        pretty_print(resp)
+    
+    def list_fields(self):
+        resp = requests.get(self.list_url, headers=self.headers)
+        resp.raise_for_status()
+        resp = resp.json()
+        pretty_print(resp)
+        pass
+
+    def get_fields_data(self) :
+        type = command_with_options('Select Field Type', self.options)
+        options = self.options_manager()
+        content = click.prompt('Enter content')
+        content = json.dumps(content)
+        return {
+            "record" : self.record_id,
+            "content" : content,
+            "type" : type,
+            "options" : options
+        }
+
+
+    
+    def options_manager(self, options={}):
+        if click.confirm('Would you like to add an option key-value?'):
+            key = click.prompt('Enter Key Name')
+            value = click.prompt(f'Enter Value for {key}')
+            options[key] = value
+            return self.options_manager(options)
+        else:
+            return json.dumps(options)
+
+        
