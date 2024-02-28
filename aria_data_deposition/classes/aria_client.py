f93e711857635479e98f6f96e14c86ba15db71ab
Lui Holliday <lui.holliday@instruct-eric.org>
2024-02-28 13:48:28 +0000

    reduce token passing

--- aria_data_deposition/classes/aria_client.py
+++ aria_data_deposition/classes/aria_client.py
@@ -8,15 +8,18 @@ class AriaClient :
     '''
     def __init__(self, login=False):
         self.client = Client(OAuth())
-        self.data_manager = DataManager()
         if not login:
             self._fetch_token()
-
+            self._get_classes()
+        
     @property
     def token(self):
         if not self._token:
             self._fetch_token()
         return self._token
+    
+    def _get_classes(self) :
+        self.data_manager = DataManager(self.token)
 
     def _fetch_token(self):
         self._token = self.client.get_access_token()['access_token']
@@ -31,19 +34,19 @@ class AriaClient :
         DoSomething.dispatch_hook(self.token)
 
     def create_bucket(self):
-        self.data_manager.create_bucket(self.token)
+        self.data_manager.create_bucket()
 
     def list_buckets(self) :
-        self.data_manager.list_buckets(self.token)
+        self.data_manager.list_buckets()
 
     def create_record(self) :
-        self.data_manager.create_record(self.token)
+        self.data_manager.create_record()
 
     def list_records(self) :
-        self.data_manager.list_records(self.token)
+        self.data_manager.list_records()
 
     def create_field(self) :
-        self.data_manager.create_field(self.token)
+        self.data_manager.create_field()
 
     def list_fields(self):
-        self.data_manager.list_fields(self.token)
+        self.data_manager.list_fields()
