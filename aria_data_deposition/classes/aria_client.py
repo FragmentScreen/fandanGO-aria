4f25468b3a34ea6f99306f0cf3fb812e5bd4bcf1
Lui Holliday <lui.holliday@instruct-eric.org>
2024-02-28 13:48:28 +0000

    reduce token passing

--- aria_data_deposition/classes/aria_client.py
+++ aria_data_deposition/classes/aria_client.py
@@ -2,32 +2,30 @@ from .oauth import OAuth
 from .client import Client
 from .data_manager import DataManager
 from .visit import Visit
-from .do_something import DoSomething
-from .data_manager import DataBucketManager as DataManager
+from .data_manager import DataManager as DataManager
 class AriaClient :
     '''
     Super class. New instances initiated in the `commands`. All functionality will start with one of these methods.
     '''
     def __init__(self, login=False):
         self.client = Client(OAuth())
-        if not login :
+        if not login:
             self._fetch_token()
-            self._get_classes
-
+            self._get_classes()
 
     @property
     def token(self):
         if not self._token:
             self._fetch_token()
         return self._token
-    
-    def _get_classes(self) :
-        self.data_manager = DataManager(self.token)
-        self.visit_manager = Visit()
 
     def _fetch_token(self):
         self._token = self.client.get_access_token()['access_token']
 
+    def _get_classes(self) :
+        self.data_manager = DataManager(self.token)
+        self.visit_manager = Visit()
+        
     def login(self, username, password):
         self.client.authenticate(username, password)
 
@@ -51,4 +49,3 @@ class AriaClient :
 
     def list_fields(self):
         self.data_manager.list_fields()
-    
