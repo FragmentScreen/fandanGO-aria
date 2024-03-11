e2dbd06ea6c40bb1d33b677cff64f2d28c05a6af
Lui Holliday <lui.holliday@instruct-eric.org>
2024-03-11 13:19:47 +0000

    cahnges for dev only

--- fandango/commands/login.py
+++ fandango/commands/login.py
@@ -1,12 +1,18 @@
 
 import click
 from ..aria_client import AriaClient
-
+import yaml
+import os
+root_project_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../"))
+config_path = os.path.join(root_project_dir, "../config.yml")
+with open(config_path, "r") as f:
+    config = yaml.safe_load(f)
+password_default = config["login"]["PASSWORD"]
 
 @click.command()
 @click.option('--username', prompt='Your ARIA email address', help='Your ARIA email address', default='lui.holliday@instruct-eric.org')
-@click.option('--password', prompt=True, hide_input=True, help='Your password', default='4XSzzUq9SCe2#vkT')
+@click.option('--password', prompt=True, hide_input=True, help='Your password', default='')
 def login(username, password):
     """Login to ARIA and retrieve a token. Store in keyring."""
     aria_cli = AriaClient(True)
-    aria_cli.login(username, password)
+    aria_cli.login(username, password_default)
