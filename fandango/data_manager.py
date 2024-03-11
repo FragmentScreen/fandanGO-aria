be4136599dd940f4d49e86bffbcac39c6bcb99d4
Lui Holliday <lui.holliday@instruct-eric.org>
2024-03-11 13:18:51 +0000

    separation in DM

--- fandango/data_manager.py
+++ fandango/data_manager.py
@@ -12,33 +12,19 @@ class DataManager:
         if bucket_id is not None :
             self.populate(bucket_id)
 
+    
+    # BUCKET
+
     def create_bucket(self, fields=None) :
         bucket = self.bucket_manager.create_bucket(fields)
         self.records_manager.bucket_id = bucket['id']
         return bucket['id']
-
-    def populate(self, bucket_id) :
-        self.bucket_manager.populate()
-        self.records_manager.bucket_id = bucket_id
-        self.records_manager.records= self.get_records_for_bucket()
-        self.fields_manager.record_id  = self.records_manager.records
-        self.fields_manager.populate_fields_for_records()
-
-
-    # def check_bucket_exists(self) :
-    #     return True if self.bucket_manager.bucket else False
-
-    def get_records_for_bucket(self) :
-        # if not self.check_bucket_exists() :
-            #  return False
-        return self.records_manager.get_records()
     
-    def get_fields_for_records(self) :
-        pretty_print(self.fields_manager.fields)
-    
-    def print_records(self) :
-        pretty_print(self.records_manager.records)
-
+    def select_bucket(self) : 
+        bucket = self.bucket_manager.select_bucket()
+        bucket_id = bucket['id']
+        self.populate(bucket_id)
+        return bucket_id
     
     def print_bucket_info(self) :
         if self.bucket_manager.bucket :
@@ -51,9 +37,33 @@ class DataManager:
             print('No bucket set')
 
 
+    # RECORDS 
+
+    def get_records_for_bucket(self) :
+        return self.records_manager.get_records()
+    
+    def print_records(self) :
+        pretty_print(self.records_manager.records)
+
     def create_record(self, fields=None):
         record_id = self.records_manager.create_record(fields)
         return record_id
+    
+    def select_record(self) :
+        record = self.records_manager.select_record()
+        self.fields_manager.record_id = record['id']
+        return self.fields_manager.record_id
+    
+    def get_fields_for_records(self) :
+        pretty_print(self.fields_manager.fields)
+    
+
+
+    
+
+
+
+
     
     def select_record(self) :
         # self.select_bucket()
@@ -69,20 +79,12 @@ class DataManager:
         self.select_record()
         self.fields.list_fields()
 
-    def select_bucket(self) : 
-        bucket = self.bucket_manager.select_bucket()
-        bucket_id = bucket['id']
-        self.populate(bucket_id)
-        return bucket_id
 
-    # def list_records(self):
-    #     self.select_bucket()
-    #     self.records.get_records()
+    # INITIATOR (When bucket_id supplier)
 
-    # def list_all_entity_fields
-        
-    # def create_bucket(self)
-        # self.bucket = new_bucket.create_bucket(fields)
-        # self.bucket.create_bucket(fields)
-
-# may want to turn these into subclasses where bucket consumes record, consumes field 
+    def populate(self, bucket_id) :
+        self.bucket_manager.populate()
+        self.records_manager.bucket_id = bucket_id
+        self.records_manager.records= self.get_records_for_bucket()
+        self.fields_manager.record_id  = self.records_manager.records
+        self.fields_manager.populate_fields_for_records()
