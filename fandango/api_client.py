8753b07f385d58e3ec41fd150e5f6b00fe268062
Lui-Instruct <164496776+Lui-Instruct@users.noreply.github.com>
2024-04-10 14:20:11 +0100

    cleanup

--- fandango/api_client.py
+++ fandango/api_client.py
@@ -3,24 +3,24 @@ from .utils import set_headers, get_config
 
 config = get_config()
 class APIClient:
-    def __init__(self,token):
+    def __init__(self, token):
         self.token = token
         self.base_url = config['apis']['DATA_DEPOSITION_BASE']
+        self.aria_login_url = config["login"]["LOGIN_URL"]
+        self.headers = self.token if self.token else {}
 
     def get(self, endpoint, params=None):
         url = f"{self.base_url}/{endpoint}"
-        headers = set_headers(self.token)
         if params:
             query_string = self._construct_query_string(params)
             url += f"?{query_string}"
-        resp = requests.get(url, headers=headers)
+        resp = requests.get(url, headers=self.headers)
         resp.raise_for_status()
         return resp.json()
 
     def post(self, endpoint : str, data : dict = None):
         url = f"{self.base_url}/{endpoint}"
-        headers = set_headers(self.token)
-        resp = requests.post(url, json=data, headers=headers)
+        resp = requests.post(url, json=data, headers=self.headers)
         resp.raise_for_status()
         return resp.json()
 
