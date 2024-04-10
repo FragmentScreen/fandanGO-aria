2bc6eb4c94d11393e845f7ec9c8774ec020335e4
Lui-Instruct <164496776+Lui-Instruct@users.noreply.github.com>
2024-04-10 14:20:28 +0100

    oauth api client class

---
+++ fandango/client_oauth.py
@@ -0,0 +1,18 @@
+from .api_client import APIClient
+from .utils import get_config
+from .config import requests
+
+config = get_config()
+
+
+class ClientOauth(APIClient) :
+
+    def __init__(self, token=None):
+        super().__init__(token)
+
+    def login(self, login_data) :
+        response = requests.post(self.aria_login_url, login_data)
+        response.raise_for_status()
+        response = response.json()
+        return response
+
