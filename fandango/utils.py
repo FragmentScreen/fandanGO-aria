85af3c61ef702c03210bf437aa0e1ce3a89f4d1c
Lui-Instruct <164496776+Lui-Instruct@users.noreply.github.com>
2024-04-11 21:09:31 +0100

    version 11

--- fandango/utils.py
+++ fandango/utils.py
@@ -49,13 +49,12 @@ def set_headers(token) :
 
 # CONFIG
 
-def get_config(config_file = 'config.yml') :
-    """Get the path to the config.yml file in FandanGO"""
-    root_project_dir = os.path.abspath(os.path.join(os.path.dirname(__file__)))
-    config_path = os.path.join(root_project_dir, "config/config.yml")
+def get_config(config_file='config.yml'):
+    """Get the path to the config.yml file"""
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+    config_path = os.path.join(current_dir, config_file)
     with open(config_path, "r") as f:
         config = yaml.safe_load(f)
-    config_path = os.path.abspath(config_file)
     return config
 
 
