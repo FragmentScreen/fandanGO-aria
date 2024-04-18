25d2c945cdd966f6bd7fd2d70027669557b435ea
Lui-Instruct <164496776+Lui-Instruct@users.noreply.github.com>
2024-04-18 16:02:58 +0100

    version 1 for pypi

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='fandango-aria-plugin',
-    version='0.2.0',
+    version='1.0.0',
     description="A Public Client library and CLI for interacting with ARIA's Data Deposition Service.",
     long_description=long_description,
     long_description_content_type='text/markdown',
