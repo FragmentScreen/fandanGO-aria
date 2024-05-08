aaafd6be4fa6f394972ddd5d3660f5dbd9ba9390
Lui-Instruct <164496776+Lui-Instruct@users.noreply.github.com>
2024-05-08 15:30:21 +0100

    conditional import for core

--- fGOaria/__init__.py
+++ fGOaria/__init__.py
@@ -1,4 +1,3 @@
-import core  # type: ignore
 from .bucket import Bucket
 from .record import Record
 from .aria_client import AriaClient
@@ -6,12 +5,19 @@ from .field import Field
 from .utils import *
 from .visit import Visit
 from .entity_manager import EntityManager
-from fGOaria.actions import deposit_field
-from fGOaria.constants import ACTION_DEPOSIT_FIELD
 
+try:
+    import core # type: ignore
+    from fGOaria.actions import deposit_field   
+    from fGOaria.constants import ACTION_DEPOSIT_FIELD
+    CORE_SOFTWARE_AVAILABLE = True
+except ImportError:
+    print_with_spaces('FandanGO Core not available, ARIA plugin acting independently...')
+    CORE_SOFTWARE_AVAILABLE = False
 
-class Plugin(core.Plugin) : 
+if CORE_SOFTWARE_AVAILABLE:
+    class Plugin(core.Plugin) : 
 
-    @classmethod
-    def define_methods(cls) : 
-        cls.define_method(ACTION_DEPOSIT_FIELD, deposit_field.perform_action)
+        @classmethod
+        def define_methods(cls) : 
+            cls.define_method(ACTION_DEPOSIT_FIELD, deposit_field.perform_action)
