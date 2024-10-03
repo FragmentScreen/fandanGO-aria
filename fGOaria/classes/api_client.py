5b84e52d1e8efbc9ded40f55f759738ce7930d98
Lui-Instruct <164496776+Lui-Instruct@users.noreply.github.com>
2024-10-03 14:43:27 +0100

    gql logic addition

--- fGOaria/classes/api_client.py
+++ fGOaria/classes/api_client.py
@@ -6,9 +6,13 @@ load_dotenv()
 class APIClient:
     def __init__(self, token):
         self.token = token
-        self.aria_login_url = os.getenv('ARIA_CONNECTION_LOGIN_URL')
+        self.dev = os.getenv('DEV', 'LOCAL')
+        if self.dev == 'LOCAL':
+            self.aria_login_url = os.getenv('ARIA_CONNECTION_LOGIN_URL_LOCAL')
+        else: 
+            self.aria_login_url = os.getenv('ARIA_CONNECTION_LOGIN_URL')
         self.headers = set_headers(self.token) if self.token else None
-        self.base_url = None 
+        self.base_url = os.getenv(f'ARIA_GQL_{self.dev}')
 
     def get(self, endpoint, params=None):
         url = f"{self.base_url}/{endpoint}"
@@ -20,10 +24,22 @@ class APIClient:
         return resp.json()
 
     def post(self, endpoint : str, data : dict = None):
-        url = f"{self.base_url}/{endpoint}"
-        resp = requests.post(url, json=data, headers=self.headers)
+        resp = requests.post(endpoint, json=data, headers=self.headers)
         resp.raise_for_status()
         return resp.json()
+    
+
+    def gql_query(self, query: str, variables: dict = None):
+        """GraphQL POST request."""
+        payload = {"query": query}
+        if variables:
+            payload["variables"] = variables
+        response = requests.post(self.base_url, json=payload, headers=self.headers)
+        json_resp = response.json()
+        if 'errors' in json_resp:
+            error_message = f"GraphQL Errors: {json_resp['errors']}"
+            raise Exception(error_message)
+        return json_resp
 
     def _construct_query_string(self, params):
         query_string_parts = []
