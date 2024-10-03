4c716f9ed3dfd592bb9e324c42f7eebbfcfc3cae
Lui-Instruct <164496776+Lui-Instruct@users.noreply.github.com>
2024-10-03 14:45:25 +0100

    import for techeval

--- fGOaria/__init__.py
+++ fGOaria/__init__.py
@@ -2,6 +2,7 @@ from .classes.bucket import Bucket
 from .classes.aria_client import AriaClient
 from .classes.field import Field
 from .classes.record import Record
+from .classes.technical_review import TechEvaluation
 from .classes.entity_manager import EntityManager
 from .utils.utility_functions import *
 from .utils.imports_config import *
