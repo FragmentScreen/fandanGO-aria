5bfc9d43cf95193b6a73696547addca75febd156
Lui-Instruct <164496776+Lui-Instruct@users.noreply.github.com>
2025-05-28 14:21:49 +0100

    version change

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='fandanGO-aria',
-    version='2.2.2',
+    version='2.2.3',
     description="ARIA connection for managing access and metadata deposition, primarily through fandanGO",
     long_description=long_description,
     long_description_content_type='text/markdown',
